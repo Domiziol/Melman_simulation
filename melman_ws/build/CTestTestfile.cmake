18b33adb305089b0860fff35300234aa496e0e76
Domiziol <domi13022001@gmail.com>
2022-10-21 16:32:07 +0200

    init

---
+++ melman_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/domiziol/melman_ws/src
+# Build directory: /home/domiziol/melman_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("melman_urdf")
---
+++ melman_ws/build/melman_urdf/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/domiziol/melman_ws/src/melman_urdf
+# Build directory: /home/domiziol/melman_ws/build/melman_urdf
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
